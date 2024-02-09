86e54e49f827f13561920b69de8e27437a307b49
doruk <doruksahin98@gmail.com>
2024-02-09 09:11:32 +0300

    interview questions

---
+++ insert.ts
@@ -0,0 +1,22 @@
+// 15 minutes
+/*
+input: array, sorted, distinct integers
+given selected number, return index to be inserted to keep array's properties same.
+*/
+
+/*
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+*/
+
+function solution(arr : number[], target: number){
+
+}
---
+++ magic_number.ts
@@ -0,0 +1,26 @@
+// 20 minutes
+/*
+input: number
+Process the number (sum of squares of its digits) until there will be 1 digit.
+When process ends, if the number is "1" return true, else false
+*/
+
+/*
+Input: n = 19
+Output: true
+Explanation:
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+
+Example 2:
+Input: n = 2
+Output: false
+Explanation:
+2^2 = 4
+*/
+
+function solution(n: number){
+
+}
