3930bef3c7440983a650c53c736974ebb045c544
doruk <doruksahin98@gmail.com>
2024-02-09 09:18:21 +0300

    interview questions

---
+++ array.ts
@@ -0,0 +1,26 @@
+/*
+input: arr: array, size: number
+Question: create sub arrays with given size.
+*/
+
+/*
+Example 1:
+Input: arr = [1,2,3,4,5], size = 1
+Output: [[1],[2],[3],[4],[5]]
+
+Example 2:
+Input: arr = [1,9,6,3,2], size = 3
+Output: [[1,9,6],[3,2]]
+
+Example 3:
+Input: arr = [8,5,3,2,6], size = 6
+Output: [[8,5,3,2,6]]
+
+Example 4:
+Input: arr = [], size = 1
+Output: []
+ */
+
+function solution(arr: number[], size: number){
+    
+}
